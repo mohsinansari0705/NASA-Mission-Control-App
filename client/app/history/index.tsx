fc8253e563bf845fcb9087485ce0ea77681ed330
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-14 01:37:36 +0530

    [client/app] history screen template created

---
+++ client/app/history/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+export default function HistoryScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>History Screen!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}

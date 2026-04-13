07b2a99051f5ce61ebcbace79b235ec693763350
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-14 01:29:09 +0530

    [client/theme] create typography.ts with custom font-family

---
+++ client/theme/typography.ts
@@ -0,0 +1,19 @@
+export const fonts = {
+  sairaStencil: 'SairaStencil_400Regular',
+  ubuntu: 'Ubuntu_400Regular',
+};
+
+export const fontSize = {
+  xs: 11,
+  sm: 13,
+  md: 15,
+  lg: 17,
+  xl: 20,
+  xxl: 24,
+};
+
+export const fontWeight = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};

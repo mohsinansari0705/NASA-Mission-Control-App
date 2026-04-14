8fec35836bf55486938a6e76e9edae2b547a6d38
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-15 00:55:49 +0530

    [server] using morgan for logging requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import cors from 'cors';
+import morgan from 'morgan';
 import express from 'express';
 
 import { planetRouter } from './routes/planets/planets.routes';
+
 
 export const app = express();
 
@@ -10,5 +12,11 @@ app.use(
     origin: 'http://localhost:8081',
   })
 );
+app.use(
+  morgan(
+    ':referrer :method :url :status :res[content-length] - :response-time ms'
+  )
+);
+
 app.use(express.json());
 app.use(planetRouter);

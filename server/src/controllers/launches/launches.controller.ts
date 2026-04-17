2aee38d7d27eadad604784f908754b7a3859f152
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-18 01:58:13 +0530

    [server/controllers] added addNewLaunch request handler

--- server/src/controllers/launches/launches.controller.ts
+++ server/src/controllers/launches/launches.controller.ts
@@ -1,6 +1,27 @@
-import { launches } from '../../models/launches.model';
+import { getLaunches, addLaunch } from '../../models/launches.model';
 
 export const getAllLaunches = (req: any, res: any) => {
-  res.status(200);
-  res.json(launches);
+  return res.status(200).json(getLaunches());
+};
+
+export const addNewLaunch = (req: any, res: any) => {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({ error: 'Missing required launch property' });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({ error: 'Invalid launch date' });
+  }
+
+  addLaunch(launch);
+
+  return res.status(201).json(launch);
 };

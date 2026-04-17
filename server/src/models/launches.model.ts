4ad43a77d0ee95399b23a2e4f9fbb40c85b43a44
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-18 01:57:08 +0530

    [server/models] added getLaunch & addLaunch functions in model

--- server/src/models/launches.model.ts
+++ server/src/models/launches.model.ts
@@ -1,14 +1,30 @@
 import type { Launch } from '../types/types';
 
-export const launches: Launch[] = [
-  {
-    flightNumber: 'GCF-001',
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('2030-05-07'),
-    destination: 'Kepler-442 b',
-    customers: ['GrayCode', 'NASA', 'SpaceX'],
-    upcoming: true,
-    success: true,
-  },
-];
+export const launches: Launch[] = [];
+
+export const getLaunches = () => {
+  return launches;
+};
+
+export const addLaunch = (launch: Partial<Launch>) => {
+  let newFlightNumber: string;
+
+  if (!launches.length) {
+    newFlightNumber = 'GCF-001';
+  } else {
+    let latestFlightNumber = launches[launches.length - 1].flightNumber;
+
+    let num = latestFlightNumber.split('-');
+    let incrementedNum = String(Number(num[1]) + 1).padStart(3, '0');
+
+    newFlightNumber = 'GCF-' + incrementedNum;
+  }
+
+  const newLaunch: Partial<Launch> = launch;
+  newLaunch.flightNumber = newFlightNumber;
+  newLaunch.customers = ['GrayCode', 'NASA'];
+  newLaunch.upcoming = true;
+  newLaunch.success = true;
+
+  launches.push(newLaunch as Launch);
+};

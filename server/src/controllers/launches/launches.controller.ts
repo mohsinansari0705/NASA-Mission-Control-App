cc460d384a1b9ae2668fb79d62677ad16f86a544
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-18 23:43:07 +0530

    [server/controllers] added abortLaunch route handler to abort launch

--- server/src/controllers/launches/launches.controller.ts
+++ server/src/controllers/launches/launches.controller.ts
@@ -1,4 +1,9 @@
-import { getLaunches, addLaunch } from '../../models/launches.model';
+import {
+  existsLaunchWithId,
+  getLaunches,
+  addLaunch,
+  abortLaunchWithId,
+} from '../../models/launches.model';
 
 export const getAllLaunches = (req: any, res: any) => {
   return res.status(200).json(getLaunches());
@@ -7,21 +12,28 @@ export const getAllLaunches = (req: any, res: any) => {
 export const addNewLaunch = (req: any, res: any) => {
   const launch = req.body;
 
-  if (
-    !launch.mission ||
-    !launch.rocket ||
-    !launch.launchDate ||
-    !launch.destination
-  ) {
-    return res.status(400).json({ error: 'Missing required launch property' });
+  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+    return res
+      .status(400)
+      .json({ ok: false, error: 'Missing required launch property' });
   }
 
   launch.launchDate = new Date(launch.launchDate);
   if (isNaN(launch.launchDate)) {
-    return res.status(400).json({ error: 'Invalid launch date' });
+    return res.status(400).json({ ok: false, error: 'Invalid launch date' });
   }
 
   addLaunch(launch);
+  return res.status(201).json({ ok: true, launch });
+};
 
-  return res.status(201).json(launch);
+export const abortLaunch = (req: any, res: any) => {
+  const launchId = req.params.id;
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(400).json({ ok: false, error: 'Launch not found' });
+  }
+
+  const launch = abortLaunchWithId(launchId);
+  return res.status(200).json({ ok: true, launch });
 };

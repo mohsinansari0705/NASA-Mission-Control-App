9e1f30f264e7c9f8ba87a9b443fa4800e965cae9
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-20 15:21:32 +0530

    [server] added jest.config.cjs to use jest in TS

---
+++ server/jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
